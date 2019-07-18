e78603bbecb1e892541a761eaf94eb1f360e2d1e
Ankur <ankur08admin@gmail.com>
2019-07-18 15:56:38 +0530

    added click and crop

--- app/src/main/java/com/android/simple/mrz/SimpleAndroidMRZActivity.java
+++ app/src/main/java/com/android/simple/mrz/SimpleAndroidMRZActivity.java
@@ -10,14 +10,17 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.StrictMode;
-import android.provider.MediaStore;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.googlecode.tesseract.android.TessBaseAPI;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -25,7 +28,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class SimpleAndroidMRZActivity extends Activity {
+public class SimpleAndroidMRZActivity extends AppCompatActivity {
 
 	public static final String PACKAGE_NAME = "com.android.simple.mrz";
 	public static final String DATA_PATH = Environment
@@ -42,7 +45,7 @@ public class SimpleAndroidMRZActivity extends Activity {
 	protected Button _button;
 	// protected ImageView _image;
 	protected EditText _field;
-	protected String _path;
+
 	protected boolean _taken;
 
 	protected static final String PHOTO_TAKEN = "photo_taken";
@@ -95,8 +98,6 @@ public class SimpleAndroidMRZActivity extends Activity {
 			}
 		}
 
-		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.main);
 
 		// _image = (ImageView) findViewById(R.id.image);
@@ -104,14 +105,15 @@ public class SimpleAndroidMRZActivity extends Activity {
 		_button = (Button) findViewById(R.id.button);
 		_button.setOnClickListener(new ButtonClickHandler());
 
-		_path = DATA_PATH + "/ocr.jpg";
-		onPhotoTaken();
+
+//		onPhotoTaken();
 	}
 
 	public class ButtonClickHandler implements View.OnClickListener {
 		public void onClick(View view) {
 			Log.v(TAG, "Starting Camera app");
 			startCameraActivity();
+
 		}
 	}
 
@@ -119,50 +121,50 @@ public class SimpleAndroidMRZActivity extends Activity {
 	// http://labs.makemachine.net/2010/03/simple-android-photo-capture/
 
 	protected void startCameraActivity() {
-        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-        StrictMode.setVmPolicy(builder.build());
-		File file = new File(_path);
-		Uri outputFileUri = Uri.fromFile(file);
-
-		final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-
-		startActivityForResult(intent, 0);
+		CropImage.activity()
+				.setGuidelines(CropImageView.Guidelines.ON)
+				.start(this);
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
 		Log.i(TAG, "resultCode: " + resultCode);
-
-		if (resultCode == -1) {
-			onPhotoTaken();
-		} else {
-			Log.v(TAG, "User cancelled");
+		if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+			CropImage.ActivityResult result = CropImage.getActivityResult(data);
+			if (resultCode == RESULT_OK) {
+				Uri resultUri = result.getUri();
+				Log.i(TAG, "resultCode: " + resultUri);
+				onPhotoTaken(resultUri.getPath());
+			} else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+				Exception error = result.getError();
+				Log.i(TAG, "resultCode: " + error);
+				error.printStackTrace();
+			}
 		}
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		outState.putBoolean(SimpleAndroidMRZActivity.PHOTO_TAKEN, _taken);
+		super.onSaveInstanceState(outState);
 	}
 
 	@Override
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		Log.i(TAG, "onRestoreInstanceState()");
 		if (savedInstanceState.getBoolean(SimpleAndroidMRZActivity.PHOTO_TAKEN)) {
-			onPhotoTaken();
+			//onPhotoTaken();
 		}
 	}
 
-	protected void onPhotoTaken() {
+	protected void onPhotoTaken(String _path) {
 		_taken = true;
 
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inSampleSize = 4;
 
-//		Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
-		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mrz);
+		Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+//		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mrz);
 
 		try {
 			ExifInterface exif = new ExifInterface(_path);
